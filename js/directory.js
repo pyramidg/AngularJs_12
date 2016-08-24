e3f52094f46eafef4d2c2147a0055b5e748cf691
studioplace <webstudioplace.com>
2016-08-24 18:04:28 -0400

    angular webapp build

---
+++ js/directory.js
@@ -0,0 +1,8 @@
+angular.module('directoryApp', [])
+    .controller('directoryController', function($scope) {
+        $scope.list = [
+          {name:'Ben', age: 44, img:'http://lorempixel.com/400/200/'},
+          {name:'Jerrey', age: 36, img:'http://lorempixel.com/400/200/'},
+          {name:'Jacob', age: 24, img:'http://lorempixel.com/400/200/'}
+          ]
+       });
